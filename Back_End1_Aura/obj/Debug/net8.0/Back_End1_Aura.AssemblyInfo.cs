f5ced6ba772380c2a5f9317c41063822edf61b1e
DaisyRacin <128150355+DaisyRacin@users.noreply.github.com>
2024-04-12 09:54:04 +0200

    Package reinstall

--- Back_End1_Aura/obj/Debug/net8.0/Back_End1_Aura.AssemblyInfo.cs
+++ Back_End1_Aura/obj/Debug/net8.0/Back_End1_Aura.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back_End1_Aura")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0d17b9df4a54f3c54e553d68dabf340031c335e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back_End1_Aura")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back_End1_Aura")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
