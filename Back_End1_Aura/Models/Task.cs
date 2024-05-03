4786524cef2565d7d801a18946996ddf77c76b02
DaisyRacin <128150355+DaisyRacin@users.noreply.github.com>
2024-05-03 15:02:51 +0200

    Opgeschoond en database geupdate

---
+++ Back_End1_Aura/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Back_End1_Aura.Models
+{
+    public class ToDoTask
+    {
+        public int TaskId { get; set; } // Primary Key
+
+        [Required]
+        [StringLength(255)] // Maximum length of Description is 255 characters
+        public string Description { get; set; } = ""; // Initialized with an empty string
+
+        public DateTime CreationDate { get; set; } // Not null
+        public DateTime DueDate { get; set; } // Not null
+        public bool Status { get; set; } // Not null
+    }
+}
